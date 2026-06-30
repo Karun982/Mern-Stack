e2a231e81c22d9d72979081dfa54a762e13f22a7
Karun <karun@Karuns-MacBook-Air.local>
2026-07-01 01:04:02 +0530

    add

--- phase_01_htmlcssjs/day_03/README.md
+++ phase_01_htmlcssjs/day_03/README.md
@@ -1,1 +1,14 @@
 ## day_03
+
+
+
+## DOM --> document object model
+it is an interface whcih creates tree like structure of html tags
+
+it let u use html tags in js
+
+        html
+        |
+    head        body
+    |             |
+    meta tile   h1 button
---
+++ phase_01_htmlcssjs/day_03/index.js
@@ -0,0 +1,24 @@
+var button = document.getElementById("submit")
+
+button.addEventListener("click",handleClick)
+
+function handleClick(){
+    // alert("Button is triggered")
+    var inputUrl=document.getElementById("url").value
+    console.log(inputUrl)
+    var title = document.getElementById("title").value
+    var rating = document.getElementById("rating").value
+
+    var movieCard = document.getElementById("movieCardDiv")
+
+    movieCard.innerHTML+=`
+        <div class="card">
+                <img src="${inputUrl}" alt="">
+                <p>Movie: ${title}</p>
+                <h3>Rating: ${rating}</h3>
+            </div>
+    `
+}
+
+// ${}
+// document.querySelector("")
